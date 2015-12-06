f645364b68d58d6fd23916687638d050a4124498
laci200270 <laci200270@gmail.com>
2015-12-06 17:16:58 +0100

    Added console utils

---
+++ LaciUtils/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaciUtils
+{
+    public class ConsoleUtils
+    {
+        private static int readNumber()
+        {
+            bool canRun = true;
+            int returnable = 0;
+            while (canRun)
+            {
+                String str = Console.ReadLine();
+                if (!isNumber(str))
+                {
+                    Console.WriteLine("Számot írjál be kérlek!");
+                }
+                else
+                {
+                    returnable = Int16.Parse(str);
+                    canRun = false;
+                }
+            }
+            return returnable;
+        }
+        private static int readNumber(String message)
+        {
+            Console.Write(message + ": ");
+            return readNumber();
+
+        }
+        private static bool isNumber(String str)
+        {
+            try
+            {
+                Int16.Parse(str);
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+        
+
+    }
+}
